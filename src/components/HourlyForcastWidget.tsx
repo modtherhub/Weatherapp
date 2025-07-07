436dbb3426a6abb9b3a83925d7f475bceacca042
modtherhub <moibhub@gmail.com>
2025-07-08 03:41:30 +0400

    create forcasthourly

--- src/components/HourlyForcastWidget.tsx
+++ src/components/HourlyForcastWidget.tsx
@@ -1,19 +1,33 @@
 
+import { Send } from 'lucide-react';
 import WeatherIcone from './WeatherIcone';
 
 
 const HourlyForcastWidget = ({data}:any) => {
     
-    const { time, temperature, date, icon, summary, wind } = data;
+    const { time, temperature, date, icon, summary, wind, precipitation } = data;
     console.log(data)
     return (
         <div className='widget'>
             <div className='day'>{date}</div>
             <div className='time'>{time}</div>
-            <div className='icon'>
-                <WeatherIcone iconNumber={icon} alt={summary}/>
+            <div className='icon-tem'>
+                <div className='icon'>
+                    <WeatherIcone iconNumber={icon} alt={summary}/>
+                </div>
+                <div className='temperature'>
+                    {Math.round(temperature)} °C
+                </div>
             </div>
-            
+            <div className='precipitation'>
+                {Math.round(precipitation.total)} mm/h
+            </div>
+            <div className='wind'>
+                <div className='speed'>{Math.round(wind.speed)} km/h</div>
+                <div className='dir '>
+                    <Send className='transform rotate-[-45deg]'/>
+                </div>
+            </div>
         </div>
   )
 }
