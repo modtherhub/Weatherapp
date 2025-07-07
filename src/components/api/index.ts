5a40591b0a6ec672dde4eb2f973bc5eb9b5250c6
modtherhub <moibhub@gmail.com>
2025-07-08 02:07:48 +0400

    create forcast

--- src/components/api/index.ts
+++ src/components/api/index.ts
@@ -1,7 +1,17 @@
 import currentWeather from './current_weather.json'
+import dailyForecast from './daily_forcast.json'
+import hourlyForecast from './hourly_forcast.json'
 
 function getCurrentWeather() {
   return currentWeather.current;
 }
 
-export { getCurrentWeather };
+function getDailyForecast() {
+  return dailyForecast.daily.data;
+}
+
+function getHourlyForecast() {
+  return hourlyForecast.hourly.data;
+}
+
+export { getCurrentWeather, getDailyForecast, getHourlyForecast };
