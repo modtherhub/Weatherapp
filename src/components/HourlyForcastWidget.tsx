6eff6a18c34b406d96a9e5aaf6d96935b62e557f
modtherhub <moibhub@gmail.com>
2025-07-08 06:12:11 +0400

    create forcast hourly complate and fix prblem

--- src/components/HourlyForcastWidget.tsx
+++ src/components/HourlyForcastWidget.tsx
@@ -33,7 +33,13 @@ const HourlyForcastWidget = ({data}:any) => {
         }).format(new Date(date).setMinutes(0)),
     }
 
-    console.log(now_date, weather_date)
+    weather_date.day = 
+        weather_date.day === now_date.day  && 
+        weather_date.time === now_date.time
+        ? 'Today'
+        : weather_date.time === '00:00' 
+        ? weather_date.day : ''
+        ;
 
     return (
         <div className='container widget flex flex-col items-center justify-between bg-white border rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out'>
